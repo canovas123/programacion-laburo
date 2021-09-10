1ed49d16dd0b77df01bcc72d16a90951984e3865
canovas123 <60201659+canovas123@users.noreply.github.com>
2021-09-10 11:39:39 -0300

    Add files via upload

---
+++ products-resapi/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+from products import products 
+
+@app.route('/ping')
+def ping():
+    return jsonify({"message":"pito"})
+
+@app.route('/products')
+def getProducts():
+    return jsonify(products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
---
+++ products-resapi/products.py
@@ -0,0 +1,5 @@
+products = [
+    {"name": "laptop", "price":1000, "quantity": 440},
+    {"name": "monitor", "price":2000, "quantity": 420},
+    {"name": "teclado", "price":5000, "quantity": 220},
+]
